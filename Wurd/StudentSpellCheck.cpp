283a840fc9325dd6f104e81d8ef820274df60a07
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-04-26 16:36:54 -0600

    Trie struct with insert delete created

--- Wurd/StudentSpellCheck.cpp
+++ Wurd/StudentSpellCheck.cpp
@@ -1,24 +1,79 @@
 #include "StudentSpellCheck.h"
 #include <string>
 #include <vector>
+#include <iostream>
+#include <fstream>
+using namespace std;
 
 SpellCheck* createSpellCheck()
 {
-	return new StudentSpellCheck;
+  return new StudentSpellCheck;
+}
+
+StudentSpellCheck::StudentSpellCheck() {
+  m_trieRoot = nullptr;
 }
 
 StudentSpellCheck::~StudentSpellCheck() {
-	// TODO
+  clear(m_trieRoot);
+
 }
 
 bool StudentSpellCheck::load(std::string dictionaryFile) {
-	return false; // TODO
+  ifstream infile(dictionaryFile);
+  if (!infile) {
+    return false;
+  }
+
+  string word;
+  while (getline(infile, word)) {
+    word.erase(remove(word.begin(), word.end(), '\r'), word.end());
+    insert(word);
+  }
+
+  return true;
 }
 
 bool StudentSpellCheck::spellCheck(std::string word, int max_suggestions, std::vector<std::string>& suggestions) {
-	return false; // TODO
+  return false; // TODO
 }
 
 void StudentSpellCheck::spellCheckLine(const std::string& line, std::vector<SpellCheck::Position>& problems) {
-	// TODO
+  // TODO
+}
+
+void StudentSpellCheck::insert(string word) {
+  if (word.size() == 0) {
+    return;
+  }
+  TrieNode* currNode = m_trieRoot;
+  for (int i = 0; i < word.size(); i++) {
+    int index = charIndex(tolower(word[i]));
+    if (!currNode->childrenNodes[index]) {
+      currNode->childrenNodes[index] = new TrieNode();
+    }
+    currNode = currNode->childrenNodes[index];
+  }
+  currNode->leaf = true;
+
+}
+
+void StudentSpellCheck::clear(TrieNode* node) {
+  if (node == nullptr) {
+    return;
+  }
+  for (int i = 0; i < CHARS; i++) {
+    clear(node->childrenNodes[i]);
+  }
+  delete node;
+  node = nullptr;
+}
+
+bool StudentSpellCheck::search(string text) const {
+
+
+}
+
+int StudentSpellCheck::charIndex(char ch) {
+  return ch - 'a';
 }
--- Wurd/StudentSpellCheck.h
+++ Wurd/StudentSpellCheck.h
@@ -8,14 +8,31 @@
 
 class StudentSpellCheck : public SpellCheck {
 public:
-    StudentSpellCheck() { }
-	virtual ~StudentSpellCheck();
-	bool load(std::string dict_file);
-	bool spellCheck(std::string word, int maxSuggestions, std::vector<std::string>& suggestions);
-	void spellCheckLine(const std::string& line, std::vector<Position>& problems);
+  StudentSpellCheck();
+  virtual ~StudentSpellCheck();
+  bool load(std::string dict_file);
+  bool spellCheck(std::string word, int maxSuggestions, std::vector<std::string>& suggestions);
+  void spellCheckLine(const std::string& line, std::vector<Position>& problems);
+  static const int CHARS = 26;
 
 private:
-
+  struct TrieNode {
+    TrieNode()
+    {
+      for (int i = 0; i < CHARS; i++)
+      {
+        childrenNodes[i] = nullptr;
+      }
+      leaf = false;
+    }
+    std::vector<TrieNode*> childrenNodes;
+    bool leaf;
+  };
+  TrieNode* m_trieRoot;
+  void insert(std::string word);
+  void clear(TrieNode* node);
+  bool search(std::string text) const;
+  int charIndex(char ch);
 };
 
 #endif  // STUDENTSPELLCHECK_H_
